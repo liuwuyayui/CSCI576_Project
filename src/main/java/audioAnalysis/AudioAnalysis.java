b0abb6da791aa39f63adeda6f1268152700d57bf
liuwuyayui <72761058+liuwuyayui@users.noreply.github.com>
2021-05-03 16:25:01 -0700

    bug fixed

--- src/main/java/audioAnalysis/AudioAnalysis.java
+++ src/main/java/audioAnalysis/AudioAnalysis.java
@@ -5,27 +5,20 @@ import javaWavFileIO.WavFileException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AudioAnalysis {
   public static final int FRAME_RATE = 30;
   public static final int SAMPLE_RATE_PER_SECOND = 48000;
   public static final int SAMPLE_RATE_PER_MILLI = 48;
-  public static final int BUFFER_SIZE = 96;
+  public static final int BUFFER_SIZE = 1600;
   
-  private long volumeSum(String wavPath, int startFrame, int endFrame) throws IOException, WavFileException {
-    int startTime = (int) Math.round(startFrame * 1000.0 / FRAME_RATE);  // in millis
-    int endTime = (int) Math.round(endFrame * 1000.0 / FRAME_RATE);
-
+  public List<Long> rmsVols(String wavPath) throws IOException, WavFileException {
+    List<Long> result = new ArrayList<>();
     
-    
-    int startWavFrame = startTime * SAMPLE_RATE_PER_MILLI;
-    int endWavFrame = endTime * SAMPLE_RATE_PER_MILLI;
-    int leftWavFrameLength = endWavFrame - startFrame;
-    
-//    System.out.printf("Shot starts at %d ms, ends at %d ms\n", startTime, endTime);
-//    System.out.printf(".wav starts at %d frame, ends at %d frame\n", startWavFrame, endWavFrame);
-    
-    // open wav file
+    // Open the wav file specified as the first argument
     WavFile wavFile = WavFile.openWavFile(new File(wavPath));
     
     // Display information about the wav file
@@ -34,52 +27,58 @@ public class AudioAnalysis {
     // Get the number of audio channels in the wav file
     int numChannels = wavFile.getNumChannels();
     
-    // Create a buffer of 96 frames
-    int[] buffer = new int[BUFFER_SIZE * numChannels];
-  
-    long rmsSum = 0;
+    // Create a buffer frames
+    int bufferSize = 1000 / FRAME_RATE * SAMPLE_RATE_PER_MILLI;
+    int[] buffer = new int[bufferSize * numChannels];
     
-    while (leftWavFrameLength > 0) {
-      int rms = 0;
-      int framesRead = 0;
-      if (leftWavFrameLength >= BUFFER_SIZE) {
-        framesRead = wavFile.readFrames(buffer, BUFFER_SIZE);
-      } else {
-        framesRead = wavFile.readFrames(buffer, leftWavFrameLength);
+    int framesRead;
+    
+    while (true) {
+      long rms = 0;
+      // Read frames into buffer
+      framesRead = wavFile.readFrames(buffer, bufferSize);
+      if (framesRead == 0) {
+        break;
       }
-      leftWavFrameLength -= framesRead;
       // Loop through frames and look for minimum and maximum value
       for (int s = 0; s < framesRead * numChannels; s += 2) {
-        rms += buffer[s] * buffer[s];
+        rms += (long) buffer[s] * buffer[s];
       }
-      rms = (int) Math.round(Math.sqrt(rms / framesRead));
+      rms = (long) Math.sqrt(rms / framesRead);
 //      System.out.println(rms);
-      rmsSum += rms;
+      result.add(rms);
     }
+    
     
     // Close the wavFile
     wavFile.close();
-//    System.out.println("Long.MAX_VALUE: " + Long.MAX_VALUE);
-//    System.out.println("Total Volume RMS Sum: " + rmsSum);
-    return rmsSum;
+    
+    return result;
   }
   
   public long normalizedVolume(String wavPath, int startFrame, int endFrame) throws IOException, WavFileException {
     if (startFrame > endFrame) {
       throw new IllegalArgumentException("Endframe must greater than startFrame");
     }
-    long sum = volumeSum(wavPath, startFrame, endFrame);
-    long result = sum / (endFrame - startFrame);
+    long result = 0;
+    List<Long> rmsVols = rmsVols(wavPath);
+    for (int i = startFrame; i <= endFrame; i++) {
+      long rmsVol = rmsVols.get(i);
+      result += rmsVol * rmsVol;
+    }
+    
+    result = (long) Math.sqrt(result / (endFrame - startFrame));
+    
     System.out.printf("Normalized Volume = %d for Shot[%d, %d]\n", result, startFrame, endFrame);
     return result;
   }
   
   public static void main(String[] args) throws IOException, WavFileException {
-    String path = "/Users/daddy/Movies/project_dataset/audio/meridian.wav";
+    String path = "/Users/daddy/Movies/project_dataset/audio/steel.wav";
     AudioAnalysis audioAnalysis = new AudioAnalysis();
-    audioAnalysis.normalizedVolume(path, 0, 100);
-    audioAnalysis.normalizedVolume(path, 101, 200);
-    audioAnalysis.normalizedVolume(path, 201, 300);
-    audioAnalysis.normalizedVolume(path, 500, 1000);
+    for (int i = 0; i < 16200; i += 200) {
+      audioAnalysis.normalizedVolume(path, i, i + 99);
+    }
+    
   }
 }

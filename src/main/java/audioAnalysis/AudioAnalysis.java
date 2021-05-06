052cefc3d43d89edca8576001b0f08cd1e05a211
Ziqing Huang <zqhuang@brandeis.edu>
2021-05-07 04:11:08 +0800

    audio

--- src/main/java/audioAnalysis/AudioAnalysis.java
+++ src/main/java/audioAnalysis/AudioAnalysis.java
@@ -3,10 +3,15 @@ package audioAnalysis;
 import javaWavFileIO.WavFile;
 import javaWavFileIO.WavFileException;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AudioAnalysis {
@@ -81,12 +86,61 @@ public class AudioAnalysis {
     System.out.print("\rShots Index: "+index);
     return result;
   }
-  
-  public static void main(String[] args) throws IOException, WavFileException {
-    String path = "/Users/daddy/Movies/project_dataset/audio/steel.wav";
-    AudioAnalysis audioAnalysis = new AudioAnalysis();
-    for (int i = 0; i < 16200; i += 200) {
-      audioAnalysis.normalizedVolume(path, i, i + 99, i);
+
+  public static final int BITS_PER_SAMPLE = 16;
+  private static final double MAX_16_BIT = 32768;
+
+  public static List<Double> read(String filename) throws IOException, UnsupportedAudioFileException, UnsupportedAudioFileException {
+    File file = new File(filename);
+    AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+    AudioFormat audioFormat = ais.getFormat();
+
+    byte[] bytes = null;
+    int bytesToRead = ais.available();
+    bytes = new byte[bytesToRead];
+    int bytesRead = ais.read(bytes);
+    int n = bytes.length;
+
+    double[] data = new double[n/4];
+    for(int i = 0; i < n/4; i++) {
+      double left = ((short) (((bytes[4 * i + 1] & 0xFF) << 8) | (bytes[4 * i + 0] & 0xFF))) / ((double) MAX_16_BIT);
+      double right = ((short) (((bytes[4 * i + 3] & 0xFF) << 8) | (bytes[4 * i + 2] & 0xFF))) / ((double) MAX_16_BIT);
+      data[i] = (left + right) / 2.0;
+      //System.out.println(data[i]);
     }
+    double total = 0;
+
+    List<Double> fResult = new ArrayList<>();
+    for(int j = 0; j < data.length; j++){
+//      if(j % 1600 == 0){
+//        System.out.println(j+" "+total/1600);
+//        fResult.add(total/1600);
+//        total = 0;
+//      }else{
+//        total += data[j];
+//      }
+
+      if(j % 48000 == 0){
+        System.out.println(j/48000+" "+total/48000);
+        fResult.add(total/48000);
+        total = 0;
+      }else {
+        total += data[j];
+      }
+    }
+    Collections.sort(fResult);
+    System.out.println(fResult);
+    return fResult;
+  }
+  
+  public static void main(String[] args) throws IOException, WavFileException, UnsupportedAudioFileException {
+    String path = "/Users/ziqinghuang/Documents/project_dataset/audio/meridian.wav";
+    AudioAnalysis.read(path);
+
+//    String path = "/Users/daddy/Movies/project_dataset/audio/steel.wav";
+//    AudioAnalysis audioAnalysis = new AudioAnalysis();
+//    for (int i = 0; i < 16200; i += 200) {
+//      audioAnalysis.normalizedVolume(path, i, i + 99, i);
+//    }
   }
 }
